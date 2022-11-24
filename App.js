f91883eb365235f6842c953f9a15e5e2641dff2c
Eduardo L. I. de Andrade <90860609+EduardoLIdeAndrade@users.noreply.github.com>
2022-11-23 21:04:02 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,5 @@
+import Navagacao from "./src/Servicos/Navegacao";
+
+export default function App() {
+  return <Navagacao />;
+}
---
+++ metro.config.js
@@ -0,0 +1,21 @@
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = config;
+
+  resolver.assetExts.push("cjs");
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  return config;
+})();
